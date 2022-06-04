0cb4f0cfefde96d26e021f4cb394c82146e85401
Patrick Everett <patrick9everett@gmail.com>
2022-06-04 12:20:20 -0400

    Clean up index html. Add placeholder files for additional pages

--- assets/js/fission.js
+++ assets/js/fission.js
@@ -1,8 +1,8 @@
-function myFunction() {
+function toggleDisplay() {
     var x = document.getElementById("fission");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
-} 
+}
